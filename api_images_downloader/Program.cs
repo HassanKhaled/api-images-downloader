b60a54bca4c2c1fdae9a5e145157b7e69c57d7da
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-07-04 15:59:03 +0300

    Add project files.

---
+++ api_images_downloader/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using (var client = new HttpClient())
+{
+    using (var s = client.GetStreamAsync("https://random-d.uk/api/168.jpg"))
+    {
+        using (var fs = new FileStream("localfile.jpg", FileMode.OpenOrCreate))
+        {
+            s.Result.CopyTo(fs);
+        }
+    }
+}
