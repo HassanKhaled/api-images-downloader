08828c76ec609516b21b7a288f5e10f3f0940226
HassanKhaled <hasan.khaled.fcis@gmail.com>
2025-07-04 16:05:48 +0300

    feat(add_remote_api_call): simply we call the remote random duck api to get a random image of a duck

--- api_images_downloader/Program.cs
+++ api_images_downloader/Program.cs
@@ -1,11 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-using (var client = new HttpClient())
+﻿
+
+
+var apiUrl = "https://random-d.uk/api/random"; // Example API endpoint
+
+using var client = new HttpClient();
+
+try
 {
-    using (var s = client.GetStreamAsync("https://random-d.uk/api/168.jpg"))
-    {
-        using (var fs = new FileStream("localfile.jpg", FileMode.OpenOrCreate))
-        {
-            s.Result.CopyTo(fs);
-        }
-    }
+    // Making the GET request
+    var response = await client.GetAsync(apiUrl);
+
+    // Ensure the request was successful
+    response.EnsureSuccessStatusCode();
+
+    // Read the response content as a string
+    var content = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(content);
+
+    Console.ReadKey();
+
 }
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"Request error: {e.Message}");
+}
+
+
+// See https://aka.ms/new-console-template for more information
+//using (var client = new HttpClient())
+//{
+//    using (var s = client.GetStreamAsync("https://random-d.uk/api/168.jpg"))
+//    {
+//        using (var fs = new FileStream("localfile.jpg", FileMode.OpenOrCreate))
+//        {
+//            s.Result.CopyTo(fs);
+//        }
+//    }
+//}
